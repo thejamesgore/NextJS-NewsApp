c62a38c1264e115133628432751c2c5f4a257def
James Gore <83005220+thejamesgore@users.noreply.github.com>
2022-12-08 00:23:53 +0000

    init graphql through stepzen

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
+
+import {categories} from '../cosntants'
 import fetchNews from '../lib/fetchNews'
 
 
 async function page() {
 
-    // const news: NewsResponse = await fetchNews(categories.join(','))
+    const news: NewsResponse = await fetchNews(categories.join(','))
 
   return (
     <div>
--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -1,11 +1,77 @@
+import {gql} from 'graphql-request'
+
+import sortNewsByImage from './sortNewsByImage'
+
 const fetchNews = async (
 
     category?: Category |  string,
     keywords?: string,
-    isDynamic?:boolean
+    isDynamic?: boolean
 
 ) => {
+    // GraphQl Query
+const query = gql`
+  query MyQuery(
+    $access_key: String!
+    $categories: String!
+    $keywords: String
+  )   {
+    myQuery(
+        access_key: $access_key
+        categories: $categories
+        countries: "gb"
+        sort: "published_desc"
+        keywords: $keywords
+        ) {
+      data {
+        author
+        category
+        country
+        description
+        image
+        language
+        published_at
+        source
+        title
+        url
+      }
+      pagination {
+        count
+        limit
+        offset
+        total
+      }
+    }
+  }`
+
+
+// Fetch function with Next.js 13 caching
+
+const res = await fetch('https://gleneagle.stepzen.net/api/corn/__graphql', {
+    method: 'POST',
+    cache: isDynamic ? "no-cache" : "default",
+    next: isDynamic ?  { revalidate: 0} : {revalidate : 60},
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`
+    },
+    body: JSON.stringify({
+        query,
+        variables: {
+            access_key: process.env.MEDIASTACK_API_KEY,
+            category: category,
+            keywords: keywords,
+        }
+    })
+
     
+})
+console.log("Loading new data form api for category >>>", category,keywords)
+
+const newResponse = await res.json()
+
+const data = sortNewsByImage(newResponse.data.myQuery)
+
 
 
 }
